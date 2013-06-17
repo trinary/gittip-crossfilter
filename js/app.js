b67f6d87f677482030394b3ec6ea51016d1aa9c2
Erik Cunningham <djtrinary@gmail.com>
2013-06-17 17:52:15 -0600

    some brushes and ordinal selection.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
-var margin = { top: 0, left: 20, bottom: 30, right: 0};
+var margin = { top: 10, left: 20, bottom: 30, right: 0};
 var width = 960;
+var height = 160;
 var marginTransform = d3.svg.transform().translate([margin.left, margin.top])
 var svg = d3.select("#content").append("svg").attr("transform",marginTransform);
 
-var xscale = 
-
 d3.json("data/paydays.json",function(data) { 
+  data.reverse(); // This is still backwards.
   data.forEach(function(i) {
     i.ts_start = new Date(i.ts_start);
     i.ts_end = new Date(i.ts_end);
   });
-  console.log(data);
   var cf = crossfilter(data);
   var all = cf.groupAll();
   var participants = cf.dimension(function(d) { return d.nparticipants} );
@@ -21,15 +20,38 @@ d3.json("data/paydays.json",function(data) {
     .rangeRoundBands([0,width])
   var picker_y = d3.scale.linear()
     .domain([0,d3.max(data, function(d) { return d.nparticipants})])
-    .range([160,0])
-  console.log(picker_y.domain());
+    .range([height,0])
   var picker = svg.append("g").classed("picker",true);
-  picker.selectAll(".bars").data(data)
+  var bars = picker.selectAll(".bars").data(data)
     .enter()
     .append("rect")
     .classed("bars",true)
     .attr("x",function(d,i) { return picker_x(i); })
-    .attr("y",function(d,i) { console.log(d.nparticipants);return picker_y(d.nparticipants); })
+    .attr("y",function(d,i) { return picker_y(d.nparticipants); })
     .attr("width",picker_x.rangeBand())
-    .attr("height", function(d,i) { return 160 - picker_y(d.nparticipants); })
+    .attr("height", function(d,i) { return height - picker_y(d.nparticipants); })
+
+    //let's get brushing!
+  svg.append("g")
+    .attr("class", "brush")
+    .call(d3.svg.brush().x(picker_x)
+      .on("brushstart", brushstart)
+      .on("brush", brushmove)
+      .on("brushend", brushend))
+    .selectAll("rect")
+    .attr("height", height);
+
+
+  function brushstart() {
+    bars.classed("selecting", true);
+  }
+
+  function brushend() {
+    bars.classed("selecting", false);
+  }
+
+  function brushmove() {
+    var s = d3.event.target.extent();
+    bars.classed("selected", function(d,i) { return s[0] <= (x = picker_x(i)) && x <= s[1]; });
+  }
 });
