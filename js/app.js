ddb1acc9df302c703d4a8c1d8034d556bf5778d1
Erik Cunningham <djtrinary@gmail.com>
2013-06-17 16:11:03 -0600

    let's start making some dimensions!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
 var svg = d3.select("#content").append("svg");
-d3.json("http://gittip.com/about/paydays.json",function(data) { 
-  console.log(data);
+d3.json("/data/paydays.json",function(data) { 
+  var cf = crossfilter(data);
+  all = cf.groupAll();
+  console.log(all);
 });
